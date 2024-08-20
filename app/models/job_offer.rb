8da9181601bd7dcd53b5a0a4caebc85a018be986
Dabrovski <wojciech.dabrovski@gmail.com>
2024-08-20 17:43:57 +0200

    [Chore] Annotate models

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: job_offers
+#
+#  id              :uuid             not null, primary key
+#  external_source :text             not null
+#  external_slug   :text
+#  title           :text             not null
+#  category        :text
+#  company_name    :text
+#  company_logo    :text
+#  seniority_level :text
+#  salary_range    :text
+#  salary_currency :text
+#  contract_types  :text             default([]), is an Array
+#  tags            :text             default([]), is an Array
+#  remote          :boolean          default(FALSE), not null
+#  published_at    :datetime
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class JobOffer < ApplicationRecord
   EXTERNAL_SOURCES = %w[nofluffjobs.com].freeze
 
