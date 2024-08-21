601afc5cdb8b1b4307888c9ccf3f2d695647b9bb
Wojciech <wojciech.dabrovski@gmail.com>
2024-08-21 18:40:01 +0200

    Chore: Add and setup solid queue jobs (#7)
    * [Chore] Add and setup solid queue jobs
    
    * [Fix] Make rubocop happy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,8 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  # Use a real queuing backend for Active Job (and separate queues per environment).
+  config.active_job.queue_adapter = :solid_queue
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
