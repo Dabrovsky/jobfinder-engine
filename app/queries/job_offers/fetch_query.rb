961185bbc8209b3ad7db8bd54c5106c107d365c2
Dabrovski <wojciech.dabrovski@gmail.com>
2024-08-23 21:35:43 +0200

    [Refactor] Add fetch query class

---
+++ app/queries/job_offers/fetch_query.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module JobOffers
+  class FetchQuery < BaseQuery
+    attribute :currency, String, default: "USD"
+    attribute :technologies, Array, default: []
+    attribute :seniority, Array, default: []
+    attribute :contracts, Array, default: []
+    attribute :keywords, Array, default: []
+
+    def search
+      search_by_tags
+      search_by_contracts
+      search_by_seniority
+      scope
+    end
+
+    private
+
+    def scope
+      @scope || JobOffer.all
+    end
+
+    def search_by_tags
+      return if technologies.blank? && keywords.blank?
+
+      tags = [technologies, keywords].flatten.join(",")
+      @scope = scope.where("tags @> ?", "{#{tags}}")
+    end
+
+    def search_by_contracts
+      return if contracts.blank?
+
+      contracts_types = contracts.join(",")
+      @scope = scope.where("contract_types @> ?", "{#{contracts_types}}")
+    end
+
+    def search_by_seniority
+      return if seniority.blank?
+
+      @scope = scope.where(seniority_level: seniority)
+    end
+  end
+end
