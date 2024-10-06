abef29abc06c0bf496d7660cd6668ac5c924c4e5
Dabrovski <wojciech.dabrovski@gmail.com>
2024-10-06 15:38:54 +0200

    [Feat] Initialize pagy gem

--- config/initializers/action_controller.rb
+++ config/initializers/action_controller.rb
@@ -1,3 +1,3 @@
 Rails.application.configure do
-  config.action_controller.always_permitted_parameters = %w(controller action format)
+  config.action_controller.always_permitted_parameters = %w(controller action format page per_page)
 end
